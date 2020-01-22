b7b523643ad0f5cd6a1a01a7399795cfca204e74
Arafat Hasan Jenin <opendoor.arafat@gmail.com>
2020-01-23 00:34:55 +0600

    librapractice added

---
+++ src/libraryPractice/matplotlib-animation-of-image-list.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import cv2
+
+fig = plt.figure()
+
+ims = []
+
+img1 = cv2.imread('../../img/circle-1.png', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('../../img/circle-2.png', cv2.IMREAD_GRAYSCALE)
+img1 = img1.astype('float')
+img2 = img2.astype('float')
+
+im = plt.imshow(img1, animated=True, cmap='gray')
+ims.append([im])
+im = plt.imshow(img2, animated=True, cmap='gray')
+ims.append([im])
+
+im = plt.imshow(img1, animated=True, cmap='gray')
+ims.append([im])
+im = plt.imshow(img2, animated=True, cmap='gray')
+ims.append([im])
+
+im = plt.imshow(img1, animated=True, cmap='gray')
+ims.append([im])
+im = plt.imshow(img2, animated=True, cmap='gray')
+ims.append([im])
+
+im = plt.imshow(img1, animated=True, cmap='gray')
+ims.append([im])
+im = plt.imshow(img2, animated=True, cmap='gray')
+ims.append([im])
+
+ani = animation.ArtistAnimation(fig,
+                                ims,
+                                interval=500,
+                                blit=True,
+                                repeat_delay=0)
+
+plt.show()
